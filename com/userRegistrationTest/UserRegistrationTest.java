b58e18c5a9f793aaa780945383981c961f1360c7
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-27 15:43:46 +0530

    User Registration Test

---
+++ com/userRegistrationTest/UserRegistrationTest.java
@@ -0,0 +1,14 @@
+package com.userRegistrationTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,3 @@
+module UserRegistrationTest {
+	requires junit;
+}
