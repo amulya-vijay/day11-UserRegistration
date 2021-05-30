36146fa3898aa67be1d291ad4285adee60b68040
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-30 11:10:49 +0530

    Lambda function implementation

--- com/userRegistrationTest/UserRegistration.java
+++ com/userRegistrationTest/UserRegistration.java
@@ -3,81 +3,69 @@ package com.userRegistrationTest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 import com.userRegistrationTest.InvalidInputException.error_type;
+
+@FunctionalInterface
+interface Validation {
+	boolean validate(String input);
+	static error_type exception(boolean function, error_type invalidinput) throws InvalidInputException {
+		
+		try {
+			if(!(function)){
+				throw new InvalidInputException();
+			}
+			}catch(InvalidInputException e) {
+				return invalidinput;
+			}
+			return InvalidInputException.error_type.No_error;
+	}
+}
+
 
 public class UserRegistration {
 	
-	
-	
-	public error_type validateFirstName(String firstName) throws InvalidInputException {
-		
-
+	//Validating First Name
+		Validation firstNameValidation = user -> {
 		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-		Matcher matcher = pattern.matcher(firstName);
-		try {
-		if(!matcher.find()) {
-			throw new InvalidInputException();
-		}
-		}catch(InvalidInputException e) {
-			return InvalidInputException.error_type.Invalid_first_name;
-		}
-		return null;
+		Matcher matcher = pattern.matcher(user);
+		return matcher.find();
+		};
 		
-	}
+		
+		
+		
+		
 	
-	public error_type validateLastName(String lastName) throws InvalidInputException {
-		
+		//Validating Last Name
+		Validation lastNameValidation = user -> {
 		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-		Matcher matcher = pattern.matcher(lastName);
-		try {
-		if(!matcher.find()) {
-			throw new InvalidInputException();
-		   }
-		}catch(InvalidInputException e) {
-			return InvalidInputException.error_type.Invalid_last_name; 	
-			}
-		return null;
-	}
-
-	public error_type validateEmailId(String emailId) throws InvalidInputException {
+		Matcher matcher = pattern.matcher(user);
+		return matcher.find();
+		};
 		
+		// Validating Email-Id
+		Validation emailIdValidation = user -> {
 		Pattern pattern = Pattern.compile("^[a-z0-9+.-_]+@[a-z.]+[comin]$");
-		Matcher matcher = pattern.matcher(emailId);
-		try {
-		if(!matcher.find()) {
-			throw new InvalidInputException();
-		}
-		}catch(InvalidInputException e) {
-			return InvalidInputException.error_type.Invalid_Email; 	
-			}
-		return null;
+		Matcher matcher = pattern.matcher(user);
+		return matcher.find();
+		};
 		
-	}
-
-	public error_type validatePhoneNumber(String phoneNum) throws InvalidInputException {
+		// Validating Phone Number
+		Validation phoneNumValidation = user -> {
 		Pattern pattern = Pattern.compile("[91]{2}[: :][6789]{1}[0-9]{9}$");
-		Matcher matcher = pattern.matcher(phoneNum);
-		
-		try {
-		if(!matcher.find()) {
-			throw new InvalidInputException();}
-		}catch(InvalidInputException e) {
-			return InvalidInputException.error_type.Invalid_mobile_number; 	
-			}
-		return null;
-	}
+		Matcher matcher = pattern.matcher(user);
+		return matcher.find();
+		};
+	
 
-	public error_type validatePassword(String password) throws InvalidInputException {
+		//Validating Password
+		Validation passwordValidation = user -> {
 		Pattern pattern = Pattern.compile("(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]){8,}");
-		Matcher matcher = pattern.matcher(password);
-	try {
-		if(matcher.find()) {
-			throw new InvalidInputException();}
-		}catch(InvalidInputException e) {
-			return InvalidInputException.error_type.Invalid_password; 	
-			}
-		return null;
-	}
+		Matcher matcher = pattern.matcher(user);
+		return matcher.find();
+		};
+	
 	
 }
 
